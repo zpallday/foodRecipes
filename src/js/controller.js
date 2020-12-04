3941ba377f3e0db618d08fcbc07c8dcd94a9e16e
Zach <zpman95@hotmail.com>
2020-12-04 11:50:54 -0600

    hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -33,9 +33,15 @@ const renderSpinner = function(paremtEl){
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id)
+
+    if (!id) return;
+
+
     renderSpinner(recipeContainer)
     const res = await fetch(
-     'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+     `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
       
@@ -146,4 +152,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['haschange', 'load'].forEach(event => window.addEventListener(event, showRecipe))
