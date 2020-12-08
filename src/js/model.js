9a4d9068b46f7a4155c3c7d4c9ab87fd6eb7ff27
Zach <zpman95@hotmail.com>
2020-12-08 16:53:14 -0600

    add queries

--- src/js/model.js
+++ src/js/model.js
@@ -1,102 +1,39 @@
-// import icons from 'url:../../img/icons.svg'; // Parcel 2
+import { async } from 'regenerator-runtime'
 
-// export default class View {
-//   _data;
+export const state = {
+    recipe: {},
+    search: {
+      query: '',
+      results: [],
+      page: 1,
+      resultsPerPage: RES_PER_PAGE,
+    },
+    bookmarks: [],
+}
 
-//   /**
-//    * Render the received object to the DOM
-//    * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
-//    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
-//    * @returns {undefined | string} A markup string is returned if render=false
-//    * @this {Object} View instance
-//    * @author Jonas Schmedtmann
-//    * @todo Finish implementation
-//    */
-//   render(data, render = true) {
-//     if (!data || (Array.isArray(data) && data.length === 0))
-//       return this.renderError();
+export const loadRecipe = async function (id) {
+    try{
+    const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+       );
+       const data = await res.json();
+         
+         if(!res.ok) throw new Error(`${data.message}  (${res.status})`)
+         console.log(res, data)
 
-//     this._data = data;
-//     const markup = this._generateMarkup();
-
-//     if (!render) return markup;
-
-//     this._clear();
-//     this._parentElement.insertAdjacentHTML('afterbegin', markup);
-//   }
-
-//   update(data) {
-//     this._data = data;
-//     const newMarkup = this._generateMarkup();
-
-//     const newDOM = document.createRange().createContextualFragment(newMarkup);
-//     const newElements = Array.from(newDOM.querySelectorAll('*'));
-//     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
-
-//     newElements.forEach((newEl, i) => {
-//       const curEl = curElements[i];
-//       // console.log(curEl, newEl.isEqualNode(curEl));
-
-//       // Updates changed TEXT
-//       if (
-//         !newEl.isEqualNode(curEl) &&
-//         newEl.firstChild?.nodeValue.trim() !== ''
-//       ) {
-//         // console.log('💥', newEl.firstChild.nodeValue.trim());
-//         curEl.textContent = newEl.textContent;
-//       }
-
-//       // Updates changed ATTRIBUES
-//       if (!newEl.isEqualNode(curEl))
-//         Array.from(newEl.attributes).forEach(attr =>
-//           curEl.setAttribute(attr.name, attr.value)
-//         );
-//     });
-//   }
-
-//   _clear() {
-//     this._parentElement.innerHTML = '';
-//   }
-
-//   renderSpinner() {
-//     const markup = `
-//       <div class="spinner">
-//         <svg>
-//           <use href="${icons}#icon-loader"></use>
-//         </svg>
-//       </div>
-//     `;
-//     this._clear();
-//     this._parentElement.insertAdjacentHTML('afterbegin', markup);
-//   }
-
-//   renderError(message = this._errorMessage) {
-//     const markup = `
-//       <div class="error">
-//         <div>
-//           <svg>
-//             <use href="${icons}#icon-alert-triangle"></use>
-//           </svg>
-//         </div>
-//         <p>${message}</p>
-//       </div>
-//     `;
-//     this._clear();
-//     this._parentElement.insertAdjacentHTML('afterbegin', markup);
-//   }
-
-//   renderMessage(message = this._message) {
-//     const markup = `
-//       <div class="message">
-//         <div>
-//           <svg>
-//             <use href="${icons}#icon-smile"></use>
-//           </svg>
-//         </div>
-//         <p>${message}</p>
-//       </div>
-//     `;
-//     this._clear();
-//     this._parentElement.insertAdjacentHTML('afterbegin', markup);
-//   }
-// }
+         const {recipe} = data.data;
+         state.recipe = {
+           id: recipe.id,
+           title: recipe.title,
+           publisher: recipe.publisher,
+           sourceUrl: recipe.source_url,
+           image: recipe.image_url,
+           servings: recipe.servings,
+           cookingTime: recipe.cooking_time,
+           ingredients: recipe.ingredients,
+         };
+         console.log(state.recipe);
+} catch (err) {
+    alert(err)
+}
+};
